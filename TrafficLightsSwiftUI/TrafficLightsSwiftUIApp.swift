ac395253d97a6bee009f13378f290afb532291f0
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-05-14 19:38:14 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by Konstantin Korchak on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
